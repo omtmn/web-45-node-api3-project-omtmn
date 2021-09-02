9a8c754a876b3b26abce1e624e9e9880276747e2
Alex <omtmn@icloud.com>
2021-09-01 22:37:18 -0400

    validate user ID

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,41 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   const timestamp = new Date().toLocaleString()
   const method = req.method
   const url = req.originalUrl
   console.log(`[${timestamp}, ${method}, ${url}]`)
-  next()ß
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if(!user){
+      res.status(400).json({
+        message: "*Gasp* User Not Found!"
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Could not find user"
+    })
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body
+  if(!name) {
+    res.status(400).json({
+      message: "missing required name field"
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const {logger} = require('./middleware/middleware')
+const { logger } = require('./middleware/middleware')
 const server = express();
+const userRouter = require('./users/users-router')
 
 server.use(express.json())
 
 server.use(logger)
+
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
